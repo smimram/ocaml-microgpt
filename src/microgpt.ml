79a6d764fe167781a804e2f057cb47022f331e0b
Samuel Mimram <smimram@gmail.com>
2026-02-23 10:47:29 +0100

    Vocab.

--- src/microgpt.ml
+++ src/microgpt.ml
@@ -16,11 +16,29 @@ end
 
 let () =
   Random.self_init ();
+  (* Let there be a Dataset docs: a list of documents (e.g. a list of names). *)
   let docs =
     File.read "input.txt"
     |> String.split_on_char '\n'
+    |> List.map String.trim
+    |> List.filter (fun s -> s <> "")
     |> List.shuffle
   in
   (* List.iter print_endline docs; *)
   Printf.printf "num docs: %d\n%!" (List.length docs);
+  (* Let there be a Tokenizer to translate strings to sequences of integers ("tokens") and back. *)
+  let uchars =
+    (* unique characters in the dataset become token ids *)
+    List.to_seq docs
+    |> Seq.concat_map String.to_seq
+    |> List.of_seq
+    |> List.sort_uniq compare
+    |> Array.of_list
+  in
+  (* token id for a special Beginning of Sequence (BOS) token *)
+  let _bos = Array.length uchars in
+  (* total number of unique tokens, +1 is for BOS *)
+  let vocab_size = Array.length uchars + 1 in
+  Printf.printf "vocab size: %d\n%!" vocab_size;
+
   ignore (Autograd.grad)
